e8eaf24d09a6a820e5f571556572ea005ee2ac1f
Adam Duff <j.adam.duff@gmail.com>
2018-08-12 21:50:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(deli)
+  if deli.length > 0
+    deli.each_with_index.map {|name, index| "#{index}. #{name}"}
+    deli.unshift "The line is currently: "
+    deli.join(" ")
+    puts deli
+  else
+    puts "The line is currently empty."
+end
